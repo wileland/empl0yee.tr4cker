44302723a5290f224c43916f5d1cf85ed74eeac0
wileland <wileland7@gmail.com>
2023-11-14 04:31:17 -0600

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
 
+// Database configuration
+const dbConfig = {
+  host: 'localhost',             // Replace with your actual database host
+  user: 'root',                  // Replace with your actual database user
+  password: 'B30wulf7',         // Replace with your actual database password
+  database: 'empl0yee_tr4cker_db'  // Replace with your actual database name
+};
+
 // Create a connection pool to the database
 const pool = mysql.createPool(dbConfig);
-  host: 'localhost',      // Replace with your actual database host
-  user: 'root',           // Replace with your actual database user
-  password: 'B30wulf7',  // Replace with your actual database password
-  database: 'empl0yee_tr4cker_db'  // Replace with your actual database name
-});
 
 // Open the MySQL connection pool
 pool.getConnection((error, connection) => {
@@ -14,7 +17,7 @@ pool.getConnection((error, connection) => {
     console.error('Error connecting to the database:', error);
     throw error;
   }
-  console.log("Successfully connected to the empl0yee_tr4cker_database.");
+  console.log('Successfully connected to the empl0yee_tr4cker_database.');
 
   // Release the connection back to the pool when done with it
   connection.release();
