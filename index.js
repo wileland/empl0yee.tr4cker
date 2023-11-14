906c16352878ead1f7e522e522b79034e0a0c772
wileland <wileland7@gmail.com>
2023-11-14 07:15:00 -0600

    updated index.js

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const dbConfig = require('./config/dbConfig'); // Create a separate configuratio
 const pool = mysql.createPool(dbConfig);
 
 // Function to start the Employee Tracker application
-async function runEmployeeTracker() {
-  let connection; // Define the connection variable
-
+async function runEmployeeTracker(connection) {
   try {
     // Get a connection from the pool
     connection = await pool.getConnection();
@@ -25,11 +23,15 @@ async function runEmployeeTracker() {
         choices: [
           'View all departments',
           'View all roles',
-          'View all employees',
+
+          
+'View all employees',
           'Add a department',
           'Add a role',
           'Add an employee',
-          'Update an employee role',
+          'Update an employee\'s role',
+          'Delete an employee',
+          'Update an employee\'s first name, last name, or manager',
           'Exit'
         ]
       }
@@ -54,8 +56,14 @@ async function runEmployeeTracker() {
       case 'Add an employee':
         await addEmployee(connection);
         break;
-      case 'Update an employee role':
+      case 'Update an employee\'s role':
         await updateEmployeeRole(connection);
+        break;
+      case 'Delete an employee':
+        await deleteEmployee(connection, answer.employee);
+        break;
+      case 'Update an employee\'s first name, last name, or manager':
+        await updateEmployee(connection, answer.employee, answer.updates);
         break;
       case 'Exit':
         closeConnection(connection);
@@ -75,7 +83,7 @@ async function viewDepartments(connection) {
   try {
     const [rows] = await connection.query('SELECT * FROM department');
     console.table(rows);
-    runEmployeeTracker();
+    runEmployeeTracker(connection);
   } catch (error) {
     console.error('Error:', error);
     closeConnection(connection);
@@ -86,11 +94,11 @@ async function viewDepartments(connection) {
 async function viewRoles(connection) {
   try {
     const query = `SELECT role.id, role.title, department.name AS department, role.salary 
-                   FROM role 
-                   INNER JOIN department ON role.department_id = department.id`;
+                  FROM role 
+                  INNER JOIN department ON role.department_id = department.id`;
     const [rows] = await connection.query(query);
     console.table(rows);
-    runEmployeeTracker();
+    runEmployeeTracker(connection);
   } catch (error) {
     console.error('Error:', error);
     closeConnection(connection);
@@ -101,13 +109,13 @@ async function viewRoles(connection) {
 async function viewEmployees(connection) {
   try {
     const query = `SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager 
-                   FROM employee e 
-                   LEFT JOIN role ON e.role_id = role.id 
-                   LEFT JOIN department ON role.department_id = department.id 
-                   LEFT JOIN employee m ON m.id = e.manager_id`;
+                  FROM employee e 
+                  LEFT JOIN role ON e.role_id = role.id 
+                  LEFT JOIN department ON role.department_id = department.id 
+                  LEFT JOIN employee m ON m.id = e.manager_id`;
     const [rows] = await connection.query(query);
     console.table(rows);
-    runEmployeeTracker();
+    runEmployeeTracker(connection);
   } catch (error) {
     console.error('Error:', error);
     closeConnection(connection);
@@ -130,156 +138,9 @@ async function addDepartment(connection) {
     });
 
     console.log(`Added ${answer.newDepartment} to departments.`);
-    runEmployeeTracker();
+    runEmployeeTracker(connection);
   } catch (error) {
     console.error('Error:', error);
     closeConnection(connection);
   }
 }
-
-// Function to add a role
-async function addRole(connection) {
-  try {
-    const departments = await connection.query('SELECT * FROM department');
-
-    const answers = await inquirer.prompt([
-      {
-        name: 'title',
-        type: 'input',
-        message: 'What is the title of the new role?'
-      },
-      {
-        name: 'salary',
-        type: 'input',
-        message: 'What is the salary of the new role?',
-        validate: value => !isNaN(value)
-      },
-      {
-        name: 'department',
-        type: 'list',
-        choices: departments[0].map(department => ({ name: department.name, value: department.id })),
-        message: 'Which department does this role belong to?'
-      }
-    ]);
-
-    await connection.query('INSERT INTO role SET ?', {
-      title: answers.title,
-      salary: answers.salary,
-      department_id: answers.department
-    });
-
-    console.log(`Added ${answers.title} to roles.`);
-    runEmployeeTracker();
-  } catch (error) {
-    console.error('Error:', error);
-    closeConnection(connection);
-  }
-}
-
-// Function to add an employee
-async function addEmployee(connection) {
-  try {
-    const [roles] = await connection.query('SELECT * FROM role');
-    const [employees] = await connection.query('SELECT * FROM employee');
-
-    const answers = await inquirer.prompt([
-      {
-        name: 'firstName',
-        type: 'input',
-        message: 'What is the employee\'s first name?'
-      },
-      {
-        name: 'lastName',
-        type: 'input',
-        message: 'What is the employee\'s last name?'
-      },
-      {
-        name: 'role',
-        type: 'list',
-        choices: roles.map(role => ({ name: role.title, value: role.id })),
-        message: 'What is the employee\'s role?'
-      },
-      {
-        name: 'manager',
-        type: 'list',
-        choices: [
-          { name: 'None', value: null },
-          ...employees.map(employee => ({ name: employee.first_name + ' ' + employee.last_name, value: employee.id }))
-        ],
-        message: 'Who is the employee\'s manager?'
-      }
-    ]);
-
-    await connection.query('INSERT INTO employee SET ?', {
-      first_name: answers.firstName,
-      last_name: answers.lastName,
-      role_id: answers.role,
-      manager_id: answers.manager
-    });
-
-    console.log(`Added ${answers.firstName} ${answers.lastName} to employees.`);
-    runEmployeeTracker();
-  } catch (error) {
-    console.error('Error:', error);
-    closeConnection(connection);
-  }
-}
-
-// Function to update an employee's role
-async function updateEmployeeRole(connection) {
-  try {
-    const [employees] = await connection.query('SELECT * FROM employee');
-    const [roles] = await connection.query('SELECT * FROM role');
-
-    const answers = await inquirer.prompt([
-      {
-        name: 'employee',
-        type: 'list',
-        choices: employees.map(employee => ({ name: employee.first_name + ' ' + employee.last_name, value: employee.id })),
-        message: 'Which employee\'s role do you want to update?'
-      },
-      {
-        name: 'role',
-        type: 'list',
-        choices: roles.map(role => ({ name: role.title, value: role.id })),
-        message: 'What is the new role of the employee?'
-      }
-    ]);
-
-    await connection.query('UPDATE employee SET role_id = ? WHERE id = ?', [answers.role, answers.employee]);
-
-    console.log('Updated employee role.');
-    runEmployeeTracker();
-  } catch (error) {
-    console.error('Error:', error);
-    closeConnection(connection);
-  }
-}
-
-// Function to close the database connection
-function closeConnection(connection) {
-  if (connection) {
-    connection.release(); // Release the connection back to the pool
-  }
-  pool.end(); // Close the pool
-}
-
-// Export the runEmployeeTracker function
-module.exports = { runEmployeeTracker };
-
-// Call the main function to start the application
-if (require.main === module) {
-  runEmployeeTracker();
-}
-
-module.exports = {
-  runEmployeeTracker,
-  viewDepartments,
-  viewRoles,
-  viewEmployees,
-  addDepartment,
-  addRole,
-  addEmployee,
-  updateEmployeeRole,
-  closeConnection
-};
