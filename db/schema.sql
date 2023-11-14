ec4432629b88fdb2dad037e58e7ad251caa408fb
wileland <wileland7@gmail.com>
2023-11-13 23:52:05 -0600

    Updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,25 +8,27 @@ DROP TABLE IF EXISTS department;
 -- Create department table
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    name VARCHAR(50) NOT NULL  -- Increased the size for department names
 );
 
 -- Create role table
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    title VARCHAR(50) NOT NULL,  -- Increased the size for role titles
+    salary DECIMAL(10, 2) NOT NULL,  -- Defined precision and scale for salary
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    -- ON DELETE SET NULL means if a department is deleted, the role's department_id will be set to NULL
 );
 
 -- Create employee table
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,  -- Increased the size for first names
+    last_name VARCHAR(50) NOT NULL,  -- Increased the size for last names
     role_id INT,
-    manager_id INT,
+    manager_id INT NULL,  -- Allowed NULL for cases where an employee has no manager
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    -- ON DELETE SET NULL means if a manager is deleted, the employee's manager_id will be set to NULL
 );
